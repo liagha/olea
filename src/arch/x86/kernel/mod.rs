e5db68a15ffa1fb3596259b9c92a6508273b2033
AliAghaahmadi <itsaliaghaahmadi@gmail.com>
2025-08-24 02:31:09 +0330

    refactor

--- src/arch/x86/kernel/mod.rs
+++ src/arch/x86/kernel/mod.rs
@@ -9,13 +9,10 @@ pub mod devices;
 pub mod boot;
 
 use bootloader::BootInfo;
-use core::arch::{asm};
+use core::arch::asm;
 
 #[cfg(target_arch = "x86_64")]
 pub(crate) static mut BOOT_INFO: Option<&'static BootInfo> = None;
-
-#[cfg(target_arch = "x86")]
-core::arch::global_asm!(include_str!("entry32.s"));
 
 pub fn register_task() {
 	let sel: u16 = 6u16 << 3;
